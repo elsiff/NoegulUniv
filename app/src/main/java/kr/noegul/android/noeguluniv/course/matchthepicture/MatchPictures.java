2185b6fe39fda0dd974239f91ef8eb94dfff4e42
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2019-11-20 19:52:30 +0900

    Game-MathPicture

---
+++ app/src/main/java/kr/noegul/android/noeguluniv/course/matchthepicture/MatchPictures.java
@@ -0,0 +1,103 @@
+package kr.noegul.android.noeguluniv.course.matchthepicture;
+
+import android.graphics.Picture;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class MatchPictures {
+    private static final String[] PICTURES = {"apple", "banana","cherry","orange","peach","strawberry","watermelon","grape","kiwi","lemon"};
+    private static final Random random = new Random();
+    private Quiz currentQuiz;
+    private int numSolved;
+    private int numFailed;
+
+    public MatchPictures() {
+        currentQuiz = generateQuiz(numSolved);
+        numSolved = 0;
+        numFailed = 0;
+    }
+
+    public void solveCurrentQuiz() {
+        numSolved++;
+        currentQuiz = generateQuiz(numSolved);
+    }
+
+    public void failCurrentQuiz() {
+        numFailed++;
+        currentQuiz = generateQuiz(numSolved);
+    }
+
+    public Quiz getCurrentQuiz() {
+        return currentQuiz;
+    }
+
+    public int getNumSolved() {
+        return numSolved;
+    }
+
+    public int getNumFailed() {
+        return numFailed;
+    }
+
+    private Quiz generateQuiz(int numSolved) {
+        List<String> pictures_name_list = new ArrayList<>();
+
+        int numPictures=8;
+        if(numSolved>10) numPictures=10;
+
+        int numAnswerPicture=random.nextInt(8);//답 하나 설정
+        String answerPictureValue=PICTURES[numAnswerPicture];
+
+        pictures_name_list.add(answerPictureValue);
+        pictures_name_list.add(answerPictureValue);//2개만 정답 쌍으로 들어감
+
+        for (int i = 0; i < numPictures-2; i++) {
+            String pictureValue= PICTURES[random.nextInt(PICTURES.length)];//하나를 뽑기
+            while(pictures_name_list.contains(pictureValue))//정답과 같거나 이미 뽑은 거면 다시 뽑기
+                pictureValue= PICTURES[random.nextInt(PICTURES.length)];
+            pictures_name_list.add(pictureValue);
+        }
+
+        List<Picture> pictures=new ArrayList<>();
+        for(int i=0;i<pictures_name_list.size();i++){
+            Picture picture=new Picture(pictures_name_list.get(i));
+            pictures.add(picture);
+        }
+        Collections.shuffle(pictures);//순서 섞어서 보여주기 위함
+
+        return new Quiz(pictures,answerPictureValue);
+    }
+
+    public class Quiz {
+        private List<Picture> pictures;
+        private final String answerPictureValue;
+
+        public Quiz(List<Picture> pictures, String answerPictureValue) {
+            this.pictures = pictures;
+            this.answerPictureValue=answerPictureValue;
+        }
+
+        public List<Picture> getPictures() {
+            return pictures;
+        }
+
+        public String getAnswer() {
+            return answerPictureValue;
+        }
+    }
+
+    public class Picture {
+        private final String value;
+
+        public Picture(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+}
--- app/src/main/java/kr/noegul/android/noeguluniv/course/matchthepicture/MatchThePictureActivity.java
+++ app/src/main/java/kr/noegul/android/noeguluniv/course/matchthepicture/MatchThePictureActivity.java
@@ -2,15 +2,189 @@ package kr.noegul.android.noeguluniv.course.matchthepicture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.noegul.android.noeguluniv.R;
+import kr.noegul.android.noeguluniv.course.CourseResult;
+import kr.noegul.android.noeguluniv.course.CourseResultActivity;
+import kr.noegul.android.noeguluniv.course.CourseTimeLimit;
 
 public class MatchThePictureActivity extends AppCompatActivity {
+    private TextView numSolvedText;
+    private MatchPictures game = new MatchPictures();
+    private CourseTimeLimit timeLimit;
+    private MatchPictures.Quiz quiz;
+    private List<MatchPictures.Picture> pictures;
+    private GridView gv;
+    List<ImageButton> clickedButtons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_the_picture);
+
+        numSolvedText = findViewById(R.id.num_solved_text);
+        gv = findViewById(R.id.gridView);
+
+        timeLimit = new CourseTimeLimit(this, (ProgressBar) findViewById(R.id.time_limit_progress));
+        timeLimit.setFinishHandler(new Runnable() {
+            @Override
+            public void run() {
+                finishWithResultActivity();
+            }
+        });
+        timeLimit.start();
+
+        setupQuizLayout();
+    }
+
+    private void finishWithResultActivity() {
+        Intent intent = new Intent();
+        intent.setClass(MatchThePictureActivity.this, CourseResultActivity.class);
+
+        intent.putExtra("num-solved", game.getNumSolved());
+        intent.putExtra("num-failed", game.getNumFailed());
+
+        CourseResult result;
+        if (game.getNumFailed() <= 3 && game.getNumSolved() >= 25)
+            result = CourseResult.PASS;
+        else
+            result = CourseResult.NON_PASS;
+        intent.putExtra("result", result);
+
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (timeLimit.hasStarted()) {
+            timeLimit.stop();
+        }
+    }
+
+    private void setupQuizLayout() {
+        numSolvedText.setText("Solved " + game.getNumSolved() + ", failed " + game.getNumFailed());
+
+        quiz = game.getCurrentQuiz();
+        pictures = quiz.getPictures();
+        clickedButtons.clear();
+
+        gv.setAdapter(new PictureGridAdapter(this, pictures));
+
+    }
+
+    private void makeChoice(List<ImageButton> clickedButton) {
+        MatchPictures.Picture picture1 =
+                (MatchPictures.Picture) clickedButton.get(0).getTag();
+        MatchPictures.Picture picture2 =
+                (MatchPictures.Picture) clickedButton.get(1).getTag();
+
+        if (quiz.getAnswer().equals(picture1.getValue()) &&
+                (quiz.getAnswer().equals(picture2.getValue()))) {
+            game.solveCurrentQuiz();
+        } else {
+            game.failCurrentQuiz();
+        }
+        setupQuizLayout();
+    }
+
+    private int imageResourceOf(MatchPictures.Picture picture) {
+        switch (picture.getValue()) {
+            case "apple":
+                return R.drawable.apple;
+            case "banana":
+                return R.drawable.banana;
+            case "cherry":
+                return R.drawable.cherry;
+            case "orange":
+                return R.drawable.orange;
+            case "peach":
+                return R.drawable.peach;
+            case "strawberry":
+                return R.drawable.strawberry;
+            case "watermelon":
+                return R.drawable.watermelon;
+            case "grape":
+                return R.drawable.grape;
+            case "kiwi":
+                return R.drawable.kiwi;
+            case "lemon":
+                return R.drawable.lemon;
+            default:
+                throw new IllegalStateException("Invalid picture");
+        }
+    }
+
+    private class PictureGridAdapter extends BaseAdapter {
+        private Context context;
+        private List<MatchPictures.Picture> pictures;
+
+        public PictureGridAdapter(Context context, List<MatchPictures.Picture> pictures) {
+            this.context = context;
+            this.pictures = pictures;
+        }
+
+        @Override
+        public int getCount() {
+            return pictures.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return pictures.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;//?
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup viewGroup) {
+            MatchPictures.Picture picture = pictures.get(i);
+
+
+            ImageButton imageButton = new ImageButton(context);
+            imageButton.setImageResource(imageResourceOf(picture));
+            imageButton.setBackgroundResource(R.color.colorLightGray);
+            //imageButton.setMaxWidth(150);
+            imageButton.setMaxHeight(150);
+            imageButton.setAdjustViewBounds(true);
+            imageButton.setPadding(1, 1, 1, 1);
+            imageButton.setTag(picture);
+
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    ImageButton button = (ImageButton) view;
+
+                    if (clickedButtons.contains(button)) {
+                        clickedButtons.remove(button);
+                        button.setBackgroundResource(R.color.colorLightGray);
+                    } else {
+                        clickedButtons.add(button);
+                        button.setBackgroundResource(R.color.colorGray);
+                    }
+                    if (clickedButtons.size() == 2) makeChoice(clickedButtons);
+                }
+            });
+            return imageButton;
+        }
     }
 }
